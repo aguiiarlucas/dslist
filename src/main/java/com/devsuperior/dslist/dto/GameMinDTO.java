bd2765a29f822cf02495192dc6f631f7a1dbbe2e
Lucas Aguiar <lucascostaaguiar@hotmail.com>
2023-07-12 19:20:40 -0300

    "Domain model"

--- src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
+++ src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
@@ -1,32 +1,32 @@
-package com.devsuperior.dslist.entities;
-
-
+package com.devsuperior.dslist.dto;
+import com.devsuperior.dslist.entities.Game;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name="tb_game")
-public class Game {
+@Table(name = "tb_game")
+public class GameMinDTO {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
-    @Column(name="game_year")
+    @Column(name = "game_year")
     private Integer year;
-    private String genre;
-    private String platforms;
-    private Double score;
     private String imgUrl;
     @Column(columnDefinition = "TEXT")
     private String shortDescription;
-    @Column(columnDefinition = "TEXT")
-    private String longDescription;
 
+    public GameMinDTO(Game entity) {
+        id = entity.getId();
+        title = entity.getTitle();
+        year = entity.getYear();
+        imgUrl = entity.getImgUrl();
+        shortDescription = entity.getShortDescription();
+    }
 
 }
--- src/main/java/com/devsuperior/dslist/repositories/GameRepository.java
+++ src/main/java/com/devsuperior/dslist/repositories/GameRepository.java
@@ -1,2 +1,9 @@
-package com.devsuperior.dslist.repositories;public class GameRepository {
+package com.devsuperior.dslist.repositories;
+
+import com.devsuperior.dslist.entities.Game;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GameRepository  extends JpaRepository<Game,Long> {
+
+
 }
